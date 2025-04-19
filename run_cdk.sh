f2e3249ee0ba338ab155b78a4ac6aed87a18b9e9
Ronal-Jesus-Condor-Blas <ronal.condor@utec.edu.pe>
2025-04-19 17:50:59 -0500

    summary

--- run_cdk.sh
+++ run_cdk.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Get temporary credentials
-CREDENTIALS=$(aws sts assume-role --role-arn arn:aws:iam::455070195064:role/LabRole --role-session-name CDKSession)
+#CREDENTIALS=$(aws sts assume-role --role-arn arn:aws:iam::455070195064:role/LabRole --role-session-name CDKSession)
 
 # Extract the credentials
-export AWS_ACCESS_KEY_ID=$(echo $CREDENTIALS | jq -r '.Credentials.AccessKeyId')
-export AWS_SECRET_ACCESS_KEY=$(echo $CREDENTIALS | jq -r '.Credentials.SecretAccessKey')
-export AWS_SESSION_TOKEN=$(echo $CREDENTIALS | jq -r '.Credentials.SessionToken')
+#export AWS_ACCESS_KEY_ID=$(echo $CREDENTIALS | jq -r '.Credentials.AccessKeyId')
+#export AWS_SECRET_ACCESS_KEY=$(echo $CREDENTIALS | jq -r '.Credentials.SecretAccessKey')
+#export AWS_SESSION_TOKEN=$(echo $CREDENTIALS | jq -r '.Credentials.SessionToken')
 
 # Set CDK_NEW_BOOTSTRAP to skip bootstrapping
 export CDK_NEW_BOOTSTRAP=1
